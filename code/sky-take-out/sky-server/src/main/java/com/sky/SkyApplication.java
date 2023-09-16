02e3dfa2236c39b7c9f817c851dc47f0a80c3b7e
sanzhixiong <hanxingchen1986@gmail.com>
2023-09-16 23:04:50 +0800

    使用Spring-Cache技术进行注解操作，根据注解操作完成相应的代码实现具体参考项目一的缓存套餐

--- code/sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ code/sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,17 +1,19 @@
 package com.sky;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+        import lombok.extern.slf4j.Slf4j;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cache.annotation.EnableCaching;
+        import org.springframework.data.redis.core.RedisTemplate;
+        import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
+@EnableCaching
 public class SkyApplication {
     private static Logger log = LoggerFactory.getLogger(SkyApplication.class);
 
